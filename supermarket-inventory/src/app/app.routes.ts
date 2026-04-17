5bf31de1a1e8c7ea5f433a568f8aaf82fa896fb6
Alexandra Gumalal <gumalal.alexandra@iisgalvanimi.edu.it>
2026-04-17 09:38:36 +0000

    Aggiunta sezione Beauty(self-care) + 5 prodotti

--- supermarket-inventory/src/app/app.routes.ts
+++ supermarket-inventory/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Home } from './pagine/home/home';
 import { Settings } from './pagine/settings/settings';
+import { ProductListComponent } from './componenti/product-list-component/product-list-component';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'settings', component: Settings },
+  { path: 'beauty', component: ProductListComponent }, // Questa è la tua "stanza" ufficiale
 ];
--- supermarket-inventory/src/app/componenti/product-list-component/product-list-component.ts
+++ supermarket-inventory/src/app/componenti/product-list-component/product-list-component.ts
@@ -1,9 +1,28 @@
 import { Component } from '@angular/core';
-
+import { Product } from '../../product';
+import { CommonModule } from '@angular/common'; // Necessario per far funzionare *ngFor
 @Component({
   selector: 'app-product-list-component',
-  imports: [],
+  imports: [CommonModule], //Per usare angular in html
   templateUrl: './product-list-component.html',
   styleUrl: './product-list-component.css',
 })
-export class ProductListComponent {}
+export class ProductListComponent {
+  // Lista Beauty(Self-Care)
+  prodotti: Product[] = [
+    { id: 1, name: 'Siero Vitamina C', price: 19.90, description: '[Skincare] Illuminante...' },
+    { id: 2, name: 'Mascara Allungante HD', price: 13.00, description: '[Make-up] Definisce...' },
+    { id: 3, name: 'E-Rasoio Precision', price: 22.00, description: '[Grooming] Ideale...' },
+    { id: 4, name: 'Shampoo Cheratina', price: 4.50, description: '[Haircare] Trattamento...' },
+    { id: 5, name: 'Rossetto Matte', price: 9.90, description: '[Make-up] Colore...' }
+  ];
+
+  // Questa variabile terrà traccia del prodotto che l'utente clicca
+  prodottoSelezionato?: Product;
+
+  // Questa funzione verrà chiamata quando un prodotto verrà cliccato nell'html
+  selezionaProdotto(p: Product): void {
+    this.prodottoSelezionato = p;
+    console.log('Prodotto selezionato:', p.name); // Utile per testare nel terminale/console
+  }
+}
